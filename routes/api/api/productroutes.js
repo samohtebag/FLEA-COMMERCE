86fe3858af981e1b6c95eca074c5addd2c61f637
etown285 <78112738+etown285@users.noreply.github.com>
2021-05-15 11:31:31 -0400

    added routes and documentation

---
+++ routes/api/api/productroutes.js
@@ -0,0 +1,144 @@
+const router = require('express').Router();
+const { Product, User} = require('../../models');
+
+// The `/api/products` endpoint
+
+// get all products
+router.get('/', async (req, res) => {
+  // find all products
+  // be sure to include its associated Category and Tag data
+  try {
+  const productData = await Product.findAll({
+    attributes: ['id', 'name', 'price', 'stock'],
+    order: [['price', 'DESC']]
+    // include: [{
+    //   model: Category,
+    //   attributes: ['id', 'category_name'],
+    // },
+    // {
+    //   model: Tag,
+    //   attributes: ['id', 'tag_name'],
+    // },
+//   ],
+  });
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
+});
+
+// get one product
+router.get('/:id', async (req, res) => {
+  // find a single product by its `id`
+  // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      attributes: ['id', 'name', 'price', 'stock'],
+//     include: [{
+//       model: Category,
+//       attributes: ['id', 'category_name'],
+//     },
+//     {
+//       model: Tag,
+//       attributes: ['id', 'tag_name'],
+//     },
+//   ],
+  });
+  if(!productData) {
+    res.status(404).json({
+      message: 'No Product found with that id!'
+    });
+    return;
+  }
+  res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// create new product
+router.post('/', (req, res) => {
+
+  Product.create({
+    name: req.body.name,
+    price: req.body.price,
+    stock: req.body.stock,
+
+  })
+    // .then((product) => {
+      // if there's product tags, we need to create pairings to bulk create in the ProductTag model
+    //   if (req.body.tagIds.length) {
+    //     const productTagIdArr = req.body.tagIds.map((tag_id) => {
+    //       return {
+    //         product_id: product.id,
+    //         tag_id,
+    //       };
+    //     });
+    //     return ProductTag.bulkCreate(productTagIdArr);
+    //   }
+      // if no product tags, just respond
+//       res.status(200).json(product);
+//     })
+//     .then((productTagIds) => res.status(200).json(productTagIds))
+//     .catch((err) => {
+//       console.log(err);
+//       res.status(400).json(err);
+//     });
+// });
+
+// update product
+router.put('/:id', (req, res) => {
+  // update product data
+  Product.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      // find all associated tags from ProductTag
+      return ProductTag.findAll({ where: { product_id: req.params.id } });
+    })
+    .then((productTags) => {
+      // get list of current tag_ids
+      const productTagIds = productTags.map(({ tag_id }) => tag_id);
+      // create filtered list of new tag_ids
+      const newProductTags = req.body.tagIds
+        .filter((tag_id) => !productTagIds.includes(tag_id))
+        .map((tag_id) => {
+          return {
+            product_id: req.params.id,
+            tag_id,
+          };
+        });
+      // figure out which ones to remove
+      const productTagsToRemove = productTags
+        .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
+        .map(({ id }) => id);
+
+      // run both actions
+      return Promise.all([
+        ProductTag.destroy({ where: { id: productTagsToRemove } }),
+        ProductTag.bulkCreate(newProductTags),
+      ]);
+    })
+    .then((updatedProductTags) => res.json(updatedProductTags))
+    .catch((err) => {
+      // console.log(err);
+      res.status(400).json(err);
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((deleteProduct) => {
+    res.json(deleteProduct);
+  })
+  .catchThrow((err) => res.json(err));
+});
+
+module.exports = router;
