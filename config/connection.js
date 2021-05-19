3a7af66814fb7e550b30a05ea3227dd7fc550f4a
etown285 <78112738+etown285@users.noreply.github.com>
2021-05-18 21:49:48 -0400

    fixed route and connection

--- config/connection.js
+++ config/connection.js
@@ -7,9 +7,10 @@ if (process.env.JAWSDB_URL) {
     sequelize = new Sequelize(process.env.JAWS_URL);
 } else {
     sequelize = new Sequelize(
-        process.env.DB_NAME, 
+        process.env.DB_NAME,
+        process.env.DB_USER,  
         process.env.DB_PASSWORD,
-        process.env.DB_USER, 
+        
         {
             host: 'localhost',
             dialect: 'mysql',
--- models/index.js
+++ models/index.js
@@ -3,27 +3,21 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-Product.belongsTo(Category, {
-  foreignKey: 'category_id',
-  onDelete: 'CASCADE'
-})
+Product.belongsTo(Category);//, {
+// //   foreignKey: 'category_id',
+// //   onDelete: 'CASCADE'
+// })
 
-Category.hasMany(Product, {
-  foreignKey: 'category_id'
-})
+Category.hasMany(Product); //{
+//   foreignKey: 'category_id'
+// })
 
 Product.belongsToMany(Tag,{
-    through: {
-        model: ProductTag, 
-    },
-  foreignKey: 'product_id'
+    through: ProductTag
 })
 
 Tag.belongsToMany(Product,{
-    through: {
-        model: ProductTag, 
-    },
-  foreignKey: 'tag_id'
+    through: ProductTag
 })
 
 module.exports = {
