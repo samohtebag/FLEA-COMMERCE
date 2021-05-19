75739c7d3e8dfcf423575855fc0f681fcd0fe684
gabe thomas <gabe.thomas@bizzabo.com>
2021-05-18 20:43:55 -0400

    seeds

--- seeds/seeds.js
+++ seeds/seeds.js
@@ -1,7 +1,33 @@
 const sequelize = require('../config/connection');
-const { User, Post, Comment } = require('../models');
+const { user, Category, Product, ProductTag, Tag } = require('../models');
 
-const userData = require('./userData.json');
-const productUserData = require('./productUserData.json');
-const idTagsData = require('./idTags.json');
-const tagNamesData = require('./tagNames.json');
+
+const categorySeed = require('./category-seed.json');
+const productSeed = require('./product-seed.json');
+const productTags = require('./productTag-seed.json');
+const tagSeed = require('./tag-seed.json');
+const userSeeds = require('./user-seed.json');
+
+const seedDatabase = async () => {
+    await sequelize.sync({ force: true });
+        console.log('\n-----DATABASE SYNCED -----\n');
+
+    await categorySeed();
+        console.log('\n-----CATEGORY SYNCED -----\n');
+
+    await productSeed();
+        console.log('\n-----PRODUCTS SYNCED -----\n');
+
+    await productTags();
+        console.log('\n-----PRODUCT TAGS SYNCED -----\n');
+
+    await tagSeed();
+        console.log('\n-----TAGS SYNCED -----\n');
+
+    await userSeeds();
+        console.log('\n-----USERS SYNCED -----\n');
+
+    process.exit(0);
+};
+
+seedDatabase();
