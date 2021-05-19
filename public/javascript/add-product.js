1908faece874f2b87dd0fd90c4981a9a913d2520
Jbarbss <jbarbanel@gmail.com>
2021-05-19 11:28:13 -0400

    added add- product, edit-product js files,added style sheets, added create-product dashboard edit-product and homepage handlebars

---
+++ public/javascript/add-product.js
@@ -0,0 +1,27 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const product_name = document.querySelector('input[name="product-name"]').value;
+    const product_content = document.querySelector('input[name="product-details"]').value;
+    const product_price = document.querySelector('input[name="product-price"]').value;
+
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      body: JSON.stringify({
+        product_name,
+        product_content,
+        product_price
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/dashboard');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('.new-product-form').addEventListener('submit', newFormHandler);
