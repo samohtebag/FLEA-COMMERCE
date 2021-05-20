602a0ad761e8b7b79b4960edf27808fea92476f4
Jbarbss <jbarbanel@gmail.com>
2021-05-20 12:45:47 -0400

    fixed typos in routes and tags

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 const categoryRoutes = require('./categoryRoutes');
 const productRoutes = require('./productRoutes');
 const userRoutes = require('./userRoutes');
+const tagRoutes = require('./tagRoutes')
 
 router.use('/users', userRoutes);
 router.use('/categories', categoryRoutes);
 router.use('/products', productRoutes);
+router.use('/tag', tagRoutes);
 
 module.exports = router;
